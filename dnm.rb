33e59b2b77ab55f2074529c88aa8661c2843395a
Umit Kayacik <ujk7107@hotmail.com>
2011-04-06 16:59:13 +0300

    move

---
+++ dnm.rb
@@ -0,0 +1,25 @@
+﻿Camping.goes :Dnm
+
+module Dnm::Controllers
+    class Index < R '/'
+        def get
+            @time = Time.now
+            render :sundial
+        end
+    end
+end
+
+module Dnm::Views
+    def layout
+        html do
+            head do
+                title {"Dene dene nereye kadar"}
+            end
+            body {self << yield}
+        end
+    end
+    
+    def sundial
+        p "Şu anda zaman : #{@time.strftime('%H%M%S')}"
+    end
+end
